c566fbf9413bde2ff22be76f897a29060e3d8b2c
dradamsadik <adamsadik573@gmail.com>
2024-02-14 18:24:14 +0100

    commit

---
+++ src/components/products/savings.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+
+export const SA = () => {
+  return (
+    <div id="cg">
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12">
+            <div className="col-xs-12">
+              <div className="about-text">
+                <br />
+                <br />
+                <br />
+                <br />
+                <h2>Ordinary Savings – Personal or group savings </h2>
+
+                <h3>‘Ordinary Savings’ Savings for the future</h3><br /><br /><h3>PRODUCT FEATURES AND CHARACTERISTICS</h3>
+                
+                <div className="cg-text">
+                  <ul>
+                    <li>Savings to safeguard impoverishment:<li></li>
+                      <p>
+                        <span>
+                          {" "}
+                          <ul>
+                          <li>
+                          To meet needs whenever it arises
+To meet any hiccup whenever it occurs
+Savings also assist to access more facilities from the bank etc
+                            </li>
+                            
+                            <li>
+                            - Open a savings account with the bank to safeguard your hard earn income.
+                            </li>
+                            <li>
+                            - Daily/weekly/monthly savings
+                            </li>
+                            <li>
+    - Lodgment could be through transfers or from places of business and homes.
+                            </li>
+                            <li>
+                            - Interest is payable on the balances on monthly basis
+                            </li>
+                                                        <li>
+
+                            – The safety of your funds is guaranteed by Nigeria Deposit Insurance Corporation (NDIC)
+                            </li><li>Please feel free to try us by opening an account 
+                                and see the difference?</li>
+            
+                          </ul>
+                          
+
+                       </span>{" "}
+                      </p>
+                    </li>
+                    <li>
+                      <p>
+                        <h3>ACCOUNT OPENING REQUIREMENTS </h3>{" "}
+                        <span>
+                          {" "}
+                          <strong> Open a savings account today:</strong>
+<ul>
+  <li>
+  - Complete application form to enter into the program
+  </li>
+  <li>
+  - Provide 1 passport photograph
+  </li>
+  <li>
+  - Copy of voters’ card, or driving license or international passport for new customers.
+BVN
+  </li>
+  <li>
+  - Other conditions may apply when the need of assistance arise
+ 
+  </li>
+</ul>
+
+
+
+                       </span>{" "}
+                      </p>
+                    </li>
+
+                    
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
